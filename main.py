43b0bb3238b98615f554db30f4536ce3393bc6ce
Diego Costa R <62116423+diegocostares@users.noreply.github.com>
2022-08-28 16:38:57 -0400

    Bug fixed

--- main.py
+++ main.py
@@ -8,40 +8,30 @@ INVALID_CHARACTERS = ["*", "+", "\\", "/", ":", "?", "\"",
 
 def recursive_replace(name: str, search: str, replace: str) -> str:
     """ remplaza todos los caracteres hasta que no quede ninguno"""
-    if search == "+": # Caso puntual
-        replace = "_"
+    if search == " .": # Caso puntual
+      replace = "."
     while search in name:
         name = name.replace(search, replace)
     return name
 
 if __name__ == "__main__":
-    # FICHERO = input("Ingrese la ruta a buscar: ")
-    FICHERO = str(input("Ingrese la ruta a corregir: ")).replace("\'", "")
-    n = 0 # Contador
-
-    for archivo in Path(FICHERO).glob("**/*"):
-        # Caso cuando el archivo comienza con un espacio
-        if archivo.name[0] == " ":
-            # definimos un new_name sin el primer espacio
-            new_name = archivo.name[1:]
-            while new_name == " ":
-                new_name = archivo.name[1:]
-            new_file = archivo.with_name(new_name)
-            os.rename(archivo, new_file)
-            n += 1
-           
+    n = 0
+    FICHERO = input("Ruta de la carpeta a revisar: ")
+    for archivo in Path(FICHERO).glob("**/*"):   
         # revisamos si los elementos de INVALID_CHARACTERS estan en archivo.name
-        if any(x in archivo.name for x in INVALID_CHARACTERS):
-            old_name = archivo.name  # str
+        if any(x in archivo.name for x in INVALID_CHARACTERS) or archivo.name[0] == " " or archivo.name[-1] == " ":
+            new_name = archivo.name
+            # Casos donde comienza o termina con espacios
+            while new_name[0] == " ":
+                new_name = new_name[1:]
+            while new_name[::-1][0] == " ":
+                new_name= new_name[:-1]
+            # Casos de los caracteres invalidos
             for invalid in INVALID_CHARACTERS:
-                old_name = recursive_replace(old_name, invalid, "")
-            new_file = archivo.with_name(old_name)
+                new_name = recursive_replace(new_name, invalid, "")
+            new_file = archivo.with_name(new_name)
             # cambiamos de nombre en el sistema
             os.rename(archivo, new_file)
-            print(archivo.name)
+            print(f"Modificado: {archivo.name}")
             n += 1
-
-        # TODO: Casos en los que queden espacios al final. EJ: "archivo    .txt" o "carpeta  "
-        print(archivo.name)
-
-    print("----+----\nCompleted", n, "elementos modificados")
+    print(f"\nCompletado: {n} elementos modificados.")
