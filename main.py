8dba1da1eb70dd00a934f4136835950c498f40cb
Diego Costa R <62116423+diegocostares@users.noreply.github.com>
2022-08-27 20:53:50 -0400

    Arreglo bug :bug:

--- main.py
+++ main.py
@@ -8,28 +8,29 @@ INVALID_CHARACTERS = ["-", "*", "\\", "/", "~", "&", ":", ";", "?", "\"",
 
 def recursive_replace(name: str, search: str, replace: str) -> str:
     """ remplaza todos los caracteres hasta que no quede ninguno"""
+    if search == "-": # Caso puntual
+        replace = "_"
     while search in name:
         name = name.replace(search, replace)
     return name
 
 
-# BUG 1: No funciona si hay mas de un INVALID_CHARACTERS
-
 if __name__ == "__main__":
     # FICHERO = input("Ingrese la ruta a buscar: ")
-    FICHERO = input("Ingrese la ruta a corregir: ")
+    FICHERO = str(input("Ingrese la ruta a corregir: ")).replace("\'", "")
+    n = 0 # Contador
 
     for archivo in Path(FICHERO).glob("**/*"):
-        # SI el archivo tiene un "-" se remplaza por " "
-        for invalid in INVALID_CHARACTERS:
-            if invalid in archivo.name:
+        # revisamos si los elementos de INVALID_CHARACTERS estan en archivo.name
+        if any(x in archivo.name for x in INVALID_CHARACTERS):
                 old_name = archivo.name  # str
-                new_name = recursive_replace(old_name, invalid, "")
-                new_name = recursive_replace(new_name, "  ", " ")
-
-                new_file = archivo.with_name(new_name)
+                for invalid in INVALID_CHARACTERS:
+                        old_name = recursive_replace(old_name, invalid, "")
+                new_file = archivo.with_name(old_name)
                 # cambiamos de nombre en el sistema
                 os.rename(archivo, new_file)
+                # print(archivo.name)
+                n += 1
 
         # Caso cuando el archivo comienza con un espacio
         if archivo.name[0] == " ":
@@ -37,6 +38,8 @@ if __name__ == "__main__":
             new_name = archivo.name[1:]
             new_file = archivo.with_name(new_name)
             os.rename(archivo, new_file)
+            n += 1
+        # TODO: Casos en los que queden espacios al final. EJ: "archivo    .txt" o "carpeta  "
+        
 
-        # print(archivo)
-    print("----+----\nCompleted")
+    print("----+----\nCompleted", n, "elementos modificados")
