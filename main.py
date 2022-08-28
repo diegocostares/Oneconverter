5c8a972d30f639238e2f0755ae8ec013567534f5
Diego Costa R <62116423+diegocostares@users.noreply.github.com>
2022-08-28 13:19:32 -0400

    new invalid characters

--- main.py
+++ main.py
@@ -2,18 +2,17 @@ import os
 from pathlib import Path
 
 
-INVALID_CHARACTERS = ["-", "*", "\\", "/", "~", "&", ":", ";", "?", "\"",
-                      ">", "<", "|", "=", "%", "#", "º", "ª", "  "]
+INVALID_CHARACTERS = ["*", "+", "\\", "/", ":", "?", "\"",
+                      ">", "<", "^", "|", "á", "é", "í", "ó", "ú", "  "]
 
 
 def recursive_replace(name: str, search: str, replace: str) -> str:
     """ remplaza todos los caracteres hasta que no quede ninguno"""
-    if search == "-": # Caso puntual
+    if search == "+": # Caso puntual
         replace = "_"
     while search in name:
         name = name.replace(search, replace)
     return name
-
 
 if __name__ == "__main__":
     # FICHERO = input("Ingrese la ruta a buscar: ")
@@ -21,26 +20,28 @@ if __name__ == "__main__":
     n = 0 # Contador
 
     for archivo in Path(FICHERO).glob("**/*"):
-        # revisamos si los elementos de INVALID_CHARACTERS estan en archivo.name
-        if any(x in archivo.name for x in INVALID_CHARACTERS):
-                old_name = archivo.name  # str
-                for invalid in INVALID_CHARACTERS:
-                    old_name = recursive_replace(old_name, invalid, "")
-                new_file = archivo.with_name(old_name)
-                # cambiamos de nombre en el sistema
-                os.rename(archivo, new_file)
-                # print(archivo.name)
-                n += 1
-
         # Caso cuando el archivo comienza con un espacio
         if archivo.name[0] == " ":
             # definimos un new_name sin el primer espacio
-            while archivo.name[0] == " ":
+            new_name = archivo.name[1:]
+            while new_name == " ":
                 new_name = archivo.name[1:]
             new_file = archivo.with_name(new_name)
             os.rename(archivo, new_file)
             n += 1
+           
+        # revisamos si los elementos de INVALID_CHARACTERS estan en archivo.name
+        if any(x in archivo.name for x in INVALID_CHARACTERS):
+            old_name = archivo.name  # str
+            for invalid in INVALID_CHARACTERS:
+                old_name = recursive_replace(old_name, invalid, "")
+            new_file = archivo.with_name(old_name)
+            # cambiamos de nombre en el sistema
+            os.rename(archivo, new_file)
+            print(archivo.name)
+            n += 1
+
         # TODO: Casos en los que queden espacios al final. EJ: "archivo    .txt" o "carpeta  "
-        
+        print(archivo.name)
 
     print("----+----\nCompleted", n, "elementos modificados")
--- readme.md
+++ readme.md
@@ -1,29 +1,46 @@
 # Oneconverter
+
 Programa que lee el arbol de archivos y carpetas que se le entrega de input, identifica archivos con nombres invalidos que One Drive no permite subir y los renombra quitando los simbolos bloqueados.
 
-
 ## Ejecusion
-````shell
+
+```shell
 python main.py
-````
+```
 
 ## Caracteres remplazados
-- \-
+
 - \*
 - \
 - /
-- ,
-- ~
-- &
 - :
-- ;
 - ?
-- ¿
 - <
 - \>
 - |
+- á
+- é
+- í
+- ó
+- ú
+
+## Caracteres opcionales
+
+Se pueden incorporar de forma complementaria los siguientes caracteres, modificando la linea 5 de `main.py`
+
+- ,
+- ~
+- &
+- ;
+- ¿
 - =
 - %
 - \#
 - º
 - ª
+
+Quedando la linea como:
+
+```python
+INVALID_CHARACTERS = ["*","\'", "~", "&", ";", "¿", "=", "%", "#", "º", "º", "+", "\\", "/", ":", "?", "\"", ">", "<", "^", "|", "  "]
+```
